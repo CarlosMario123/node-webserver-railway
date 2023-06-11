7f74ebb29f03df225c532628ee14a63ca3c84b59
Pablo Cesar Altuzar Grajales <pablocesaraltuzar04@gmail.com>
2023-06-11 02:07:59 -0600

    react app lista

--- app.js
+++ app.js
@@ -1,51 +1,34 @@
-const express = require("express");
-const hbs = require('hbs');
-//Para usar el archivo env
+const express = require('express');
+const app = express();
 require('dotenv').config();
 
-const app = express();
-
-//asi se usa el archico
-const port = process.env.PORT; 
+const hbs = require('hbs');
 
 
+const port = process.env.PORT || 3000;
 
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
 
 
-app.get("/", (req, res) => {
-  res.render('home', {
-    nombre: 'pablo',
-    titulo:'curso node'
-  }); 
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'fernando'
+    });
 });
 
-app.get('/generic', (require,response)=>{
-  response.render('generic',{
-    nombre: 'pablo',
-    titulo:'curso node'
-  })
-})
+app.get('/about', (req, res) => {
 
-app.get('/elements', (require,response)=>{
-  response.render('elements',{
-    nombre: 'pablo',
-    titulo:'curso node'
-  })
-})
-
-
-
-
-
-//Servir contenido estatico
-//Ejecutar lo siguiente
-app.use(express.static('public'))
-
-
-
+    res.render('about');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
